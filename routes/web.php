c30965251ff305d68f3b6e2519eb3154880db1db
Romulos II Amistad <amistadromulosII@gmail.com>
2022-07-18 17:59:17 +0800

    #10 Update web routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\CompaniesController;
 use App\Http\Controllers\EmployeesController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,11 +23,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::group(['middleware' => 'prevent-back-history'], function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resource('profile', ProfileController::class);
+    Route::resource('loans', ProfileController::class);
+    Route::resource('companies', CompaniesController::class);
+    Route::resource('users', UserController::class);
+    Route::get('/getUsers', [App\Http\Controllers\UserController::class, 'getUsers']);
+});
 Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::resource('profile', ProfileController::class);
-Route::resource('loans', ProfileController::class);
-Route::resource('companies', CompaniesController::class);
-Route::resource('employees', EmployeesController::class);
